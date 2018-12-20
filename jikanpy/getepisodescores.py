18c3a6e2d5789d7857e8fbbeec028eddebfa2b81
Valerie Hellmer <valerie.hellmer@gmail.com>
2018-12-20 15:42:29 -0600

    Graphing functionality added, but keeps failing on at different episodes

--- jikanpy/getepisodescores.py
+++ jikanpy/getepisodescores.py
@@ -1,27 +1,43 @@
 from jikanpy import Jikan
 from bs4 import BeautifulSoup
 import requests
+import matplotlib.pylab as plt
 
 jikan = Jikan()
 
+anime = jikan.search(search_type='anime', query='darlin in the franxx')
+title = anime["results"][0]["title"]
+print(title)
+mal_id = anime["results"][0]["mal_id"]
 
-tokyo = jikan.search(search_type='anime', query='tokyo ghoul')
-id = tokyo["results"][0]["mal_id"]
-#for itm in jikan.anime(id, extension="episodes")["episodes"]:
-itm = jikan.anime(id, extension="episodes")["episodes"][0]
-print(itm["forum_url"] + "&pollresults=1")
-url = itm["forum_url"] + "&pollresults=1"
-xpath = '//*[@id="contentWrapper"]/div[4]/table/tbody/tr[1]/td[3]'
-response = requests.get(url)
+rating_list = []
+ep_count = 0
 
-soup = BeautifulSoup(response.text)
+for episode in jikan.anime(mal_id, extension="episodes")["episodes"]:
+    ep_count += 1
 
-results = soup.find_all('td')
-five_rating = int(results[2].text)
-four_rating = int(results[6].text)
-three_rating = int(results[10].text)
-two_rating = int(results[14].text)
-one_rating = int(results[18].text)
-total_votes = five_rating + four_rating + three_rating + two_rating + one_rating
-avg_ep_rating = float(5 * five_rating + 4 * four_rating + 3 * three_rating + 2 * two_rating + 1 * one_rating)/total_votes
-print(avg_ep_rating)
+    url = episode["forum_url"] + "&pollresults=1"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content)
+    results = soup.find_all('td')
+
+    five_rating = int(results[2].text)
+    four_rating = int(results[6].text)
+    three_rating = int(results[10].text)
+    two_rating = int(results[14].text)
+    one_rating = int(results[18].text)
+
+    total_votes = five_rating + four_rating + three_rating + two_rating + one_rating
+    avg_ep_rating = float(5 * five_rating + 4 * four_rating + 3 * three_rating + 2 * two_rating + 1 * one_rating)/total_votes
+    print(avg_ep_rating)
+
+    rating_list.append(avg_ep_rating)
+
+plt.title(title)
+plt.xlabel("Episode")
+plt.ylabel("Rating")
+plt.plot(range(1, ep_count+1), rating_list)
+plt.show()
+
+
+
